4de53f8db640a72f5594caeab3e1de9726ae9f8b
OmerSanli <omer@arthylabs.com>
2025-05-19 21:55:34 +0300

    fix: proper tailwind postcss config for Netlify

--- statsbase-frontend/postcss.config.cjs
+++ statsbase-frontend/postcss.config.cjs
@@ -1,6 +1,6 @@
-const tailwind = require('tailwindcss')
+const tailwindcss = require('@tailwindcss/postcss')
 const autoprefixer = require('autoprefixer')
 
 module.exports = {
-  plugins: [tailwind, autoprefixer],
+  plugins: [tailwindcss, autoprefixer],
 }
